62a4506943086b8202263dba492d4581e33df725
jspark311 <josh.lindsay@gmail.com>
2020-02-22 16:20:37 -0700

    Experiment that I think I dislike.

--- src/Motherflux0r.ino
+++ src/Motherflux0r.ino
@@ -183,6 +183,9 @@ static uint32_t last_interaction  = 0;      // millis() when the user last inter
 static uint32_t disp_update_last  = 0;      // millis() when the display last updated.
 static uint32_t disp_update_next  = 0;      // millis() when the display next updates.
 static uint32_t disp_update_rate  = 1;      // Update in Hz for the display
+static uint32_t disp_selector_off = 0;      // millis() when the display dialog should timeout.
+
+DataVis current_data_vis = DataVis::TEXT;
 
 
 /* Console junk... */
@@ -586,6 +589,7 @@ void redraw_touch_test_window() {
 * Draws the tricorder app.
 */
 void redraw_tricorder_window() {
+  uint16_t data_max_width = 96;
   if (drawn_app != active_app) {
     redraw_app_window("Tricorder", 0, 0);
     dirty_slider = true;
@@ -602,8 +606,61 @@ void redraw_tricorder_window() {
     dirty_slider = false;
   }
 
+  if (disp_selector_off > millis()) {
+    data_max_width = 79;
+    // We are drawing the selector.
+    if (dirty_button) {
+      bool up   = touch->buttonPressed(1);
+      bool down = touch->buttonPressed(4);
+      uint8_t cvis = (uint8_t) current_data_vis;
+      if ((up && !down) && (cvis < 5)) {
+        cvis++;
+        draw_data_view_selector(80, 10, 16, 54,
+          DataVis::GRAPH, DataVis::VECTOR, DataVis::COMPASS, DataVis::FIELD, DataVis::TEXT, DataVis::NONE,
+          (DataVis) cvis
+        );
+      }
+      else if ((!up && down) && (cvis > 1)) {
+        cvis--;
+        draw_data_view_selector(80, 10, 16, 54,
+          DataVis::GRAPH, DataVis::VECTOR, DataVis::COMPASS, DataVis::FIELD, DataVis::TEXT, DataVis::NONE,
+          (DataVis) cvis
+        );
+      }
+      else {
+      }
+      current_data_vis = (DataVis) cvis;
+      disp_selector_off = millis() + 1500;   // Punch the timeout.
+    }
+  }
+  else if (dirty_button) {
+    if (touch->buttonPressed(4)) {
+      disp_selector_off = millis() + 1500;   // Punch the timeout.
+      data_max_width = 79;
+      draw_data_view_selector(80, 10, 16, 54,
+        DataVis::GRAPH, DataVis::VECTOR, DataVis::COMPASS, DataVis::FIELD, DataVis::TEXT, DataVis::NONE,
+        current_data_vis
+      );
+    }
+  }
+
+
   if (touch->sliderValue() <= 7) {
     // Baro
+    switch (current_data_vis) {
+      case DataVis::GRAPH:
+        break;
+      case DataVis::VECTOR:
+        break;
+      case DataVis::COMPASS:
+        break;
+      case DataVis::FIELD:
+        break;
+      case DataVis::TEXT:
+        break;
+      default:
+        break;
+    }
     if (graph_array_humidity.dirty()) {
       draw_graph_obj(
         0, 10, 96, 37, 0x03E0,
@@ -655,9 +712,23 @@ void redraw_tricorder_window() {
   }
   else if (touch->sliderValue() <= 22) {
     // TSL2561
+    switch (current_data_vis) {
+      case DataVis::GRAPH:
+        break;
+      case DataVis::VECTOR:
+        break;
+      case DataVis::COMPASS:
+        break;
+      case DataVis::FIELD:
+        break;
+      case DataVis::TEXT:
+        break;
+      default:
+        break;
+    }
     if (graph_array_visible.dirty()) {
       draw_graph_obj(
-        0, 10, 96, 45, 0xF100,
+        0, 10, data_max_width, 54, 0xF100,
         true, touch->buttonPressed(1), touch->buttonPressed(4),
         &graph_array_visible
       );
@@ -685,47 +756,61 @@ void redraw_tricorder_window() {
     display.print(graph_array_ana_light.value());
   }
   else if (touch->sliderValue() <= 37) {
-    if (touch->buttonPressed(5)) {
-      // UVI
-      if (graph_array_uvi.dirty()) {
-        draw_graph_obj(
-          0, 10, 96, 45, 0xF81F,
-          true, touch->buttonPressed(1), touch->buttonPressed(4),
-          &graph_array_uvi
-        );
-        display.setTextSize(0);
-        display.setCursor(0, 56);
-        display.setTextColor(WHITE);
-        display.print("UVI:  ");
-        display.setTextColor(0xF81F, BLACK);
-        display.print(graph_array_uvi.value());
-      }
-    }
-    else {
-      if (graph_array_uva.dirty()) {
-        draw_graph_obj(
-          0, 10, 48, 45, 0x781F,
-          true, touch->buttonPressed(1), touch->buttonPressed(4),
-          &graph_array_uva
-        );
-      }
-      if (graph_array_uvb.dirty()) {
-        draw_graph_obj(
-          48, 10, 48, 45, 0xF80F,
-          true, touch->buttonPressed(1), touch->buttonPressed(4),
-          &graph_array_uvb
-        );
-      }
-      display.setTextSize(0);
-      display.setCursor(0, 56);
-      display.setTextColor(WHITE);
-      display.print("UVA/B: ");
-      display.setTextColor(0x781F, BLACK);
-      display.print(graph_array_uva.value());
-      display.setTextColor(WHITE, BLACK);
-      display.print(" / ");
-      display.setTextColor(0xF80F, BLACK);
-      display.print(graph_array_uvb.value());
+    // UVI
+    switch (current_data_vis) {
+      case DataVis::GRAPH:
+        if (touch->buttonPressed(5)) {
+          if (graph_array_uvi.dirty()) {
+            draw_graph_obj(
+              0, 10, data_max_width, 54, 0xF81F,
+              true, touch->buttonPressed(4), touch->buttonPressed(5),
+              &graph_array_uvi
+            );
+            display.setTextSize(0);
+            display.setCursor(0, 56);
+            display.setTextColor(WHITE);
+            display.print("UVI:  ");
+            display.setTextColor(0xF81F, BLACK);
+            display.print(graph_array_uvi.value());
+          }
+        }
+        else {
+          if (graph_array_uva.dirty()) {
+            draw_graph_obj(
+              0, 10, data_max_width << 1, 54, 0x781F,
+              true, touch->buttonPressed(4), touch->buttonPressed(5),
+              &graph_array_uva
+            );
+          }
+          if (graph_array_uvb.dirty()) {
+            draw_graph_obj(
+              data_max_width << 1, 10, data_max_width << 1, 54, 0xF80F,
+              true, touch->buttonPressed(4), touch->buttonPressed(5),
+              &graph_array_uvb
+            );
+          }
+        }
+        break;
+      case DataVis::TEXT:
+        if (graph_array_uvi.dirty()) {
+          display.setTextSize(0);
+          display.setCursor(0, 12);
+          display.setTextColor(WHITE, BLACK);
+          display.print("UVI:  ");
+          display.setTextColor(0xF81F, BLACK);
+          display.println(graph_array_uvi.value());
+          display.setTextColor(WHITE, BLACK);
+          display.print("UVA:  ");
+          display.setTextColor(0x781F, BLACK);
+          display.println(graph_array_uva.value());
+          display.setTextColor(WHITE, BLACK);
+          display.print("UVB:  ");
+          display.setTextColor(0xF80F, BLACK);
+          display.print(graph_array_uvb.value());
+        }
+        break;
+      default:
+        break;
     }
   }
   else if (touch->sliderValue() <= 45) {
@@ -749,30 +834,40 @@ void redraw_tricorder_window() {
     display.setCursor(0, 0);
     display.setTextColor(0xFC00, BLACK);
     display.print("Magnetometer");
-    if (!touch->buttonPressed(5)) {
-      display.setTextColor(WHITE, BLACK);
-      Vector3f64* mag_vect = magneto.getFieldVector();
-      float bearing_north = 0.0;
-      float bearing_mag = 0.0;
-      magneto.getBearing(HeadingType::TRUE_NORTH, &bearing_north);
-      magneto.getBearing(HeadingType::MAGNETIC_NORTH, &bearing_mag);
-      draw_compass(0, 11, 44, 44, false, touch->buttonPressed(4), bearing_mag, bearing_north);
-      display.setCursor(0, 57);
-      display.print(mag_vect->length());
-      display.print(" uT");
-    }
-    else {
-      draw_graph_obj(
-        0, 10, 96, 45, 0xF81F,
-        true, touch->buttonPressed(1), touch->buttonPressed(4),
-        &graph_array_mag_confidence
-      );
-      display.setTextSize(0);
-      display.setCursor(0, 56);
-      display.setTextColor(WHITE);
-      display.print("Confidence: ");
-      display.setTextColor(0xFC00, BLACK);
-      display.print(graph_array_mag_confidence.value());
+    Vector3f64* mag_vect = magneto.getFieldVector();
+    float bearing_north = 0.0;
+    float bearing_mag = 0.0;
+    switch (current_data_vis) {
+      case DataVis::GRAPH:
+        draw_graph_obj(
+          0, 10, data_max_width, 54, 0xF81F,
+          true, touch->buttonPressed(4), touch->buttonPressed(5),
+          &graph_array_mag_confidence
+        );
+        break;
+      case DataVis::VECTOR:
+        break;
+      case DataVis::COMPASS:
+        display.setTextColor(WHITE, BLACK);
+        magneto.getBearing(HeadingType::TRUE_NORTH, &bearing_north);
+        magneto.getBearing(HeadingType::MAGNETIC_NORTH, &bearing_mag);
+        draw_compass(0, 11, 54, 54, false, touch->buttonPressed(4), bearing_mag, bearing_north);
+        break;
+      case DataVis::FIELD:
+        break;
+      case DataVis::TEXT:
+        display.setTextSize(0);
+        display.setCursor(0, 12);
+        display.setTextColor(0xFC00, BLACK);
+        display.print(mag_vect->length());
+        display.setTextColor(WHITE, BLACK);
+        display.println(" uT");
+        display.print("STDEV: ");
+        display.setTextColor(0xFC00, BLACK);
+        display.print(graph_array_mag_confidence.value());
+        break;
+      default:
+        break;
     }
   }
   else {    // Thermopile
@@ -1019,179 +1114,6 @@ void render_button_icon(uint8_t sym, int x, int y, uint16_t color) {
   }
 }
 
-
-/*
-* Given a data array, and parameters for the graph, draw the data to the
-*   display.
-*/
-void draw_graph_obj(
-  int x, int y, int w, int h, uint16_t color,
-  bool draw_base, bool draw_v_ticks, bool draw_h_ticks,
-  float* dataset, uint32_t data_len
-) {
-  if (draw_base) {   // Draw the basic frame and axes?
-    display.drawFastVLine(x, y, h, WHITE);
-    display.drawFastHLine(x, y+h, w, WHITE);
-  }
-  display.fillRect(x+1, y, w, h-1, BLACK);
-  if (w < (int32_t) data_len) {
-    dataset += (data_len - w);
-    data_len = w;
-  }
-
-  float v_max = 0.0;
-  float v_min = 0.0;
-  for (uint32_t i = 0; i < data_len; i++) {
-    float tmp = *(dataset + i);
-    v_max = strict_max(v_max, tmp);
-    v_min = strict_min(v_min, tmp);
-  }
-  float h_scale = data_len / w;
-  float v_scale = (v_max - v_min) / h;
-  for (uint32_t i = 0; i < data_len; i++) {
-    uint8_t tmp = *(dataset + i) / v_scale;
-    display.writePixel(i + x, (y+h)-tmp, color);
-  }
-  if (draw_v_ticks) {
-    display.drawFastHLine(x+1, y, 2, WHITE);
-    display.setCursor(x+2, y);
-    display.setTextColor(WHITE);
-    display.print(v_max);
-    display.setCursor(x+2, (y+h) - 9);
-    display.print(v_min);
-  }
-  if (draw_h_ticks) {
-    float last_datum = *(dataset + (data_len-1));
-    uint8_t tmp = last_datum / v_scale;
-    //display.fillCircle(x+w, tmp+y, 1, color);
-    display.setCursor(x, strict_min((uint16_t) ((y+h)-tmp), (uint16_t) (h-1)));
-    display.setTextColor(color);
-    display.print(*(dataset + (data_len-1)));
-  }
-}
-
-
-/*
-* Given a filter object, and parameters for the graph, draw the data to the
-*   display.
-*/
-void draw_graph_obj(
-  int x, int y, int w, int h, uint16_t color,
-  bool draw_base, bool draw_v_ticks, bool draw_h_ticks,
-  SensorFilter<float>* filt
-) {
-  const uint16_t DATA_SIZE = filt->windowSize();
-  const uint16_t LAST_SIDX = filt->lastIndex();
-  const float*   F_MEM     = filt->memPtr();
-  float tmp_data[DATA_SIZE];
-  for (uint16_t i = 0; i < DATA_SIZE; i++) {
-    tmp_data[i] = *(F_MEM + ((i + LAST_SIDX) % DATA_SIZE));
-  }
-  draw_graph_obj(x, y, w, h, color, draw_base, draw_v_ticks, draw_h_ticks, tmp_data, (uint32_t) DATA_SIZE);
-}
-
-/*
-* Given a filter object, and parameters for the graph, draw the data to the
-*   display.
-*/
-void draw_graph_obj(
-  int x, int y, int w, int h, uint16_t color,
-  bool draw_base, bool draw_v_ticks, bool draw_h_ticks,
-  SensorFilter<uint32_t>* filt
-) {
-  const uint16_t  DATA_SIZE = filt->windowSize();
-  const uint16_t  LAST_SIDX = filt->lastIndex();
-  const uint32_t* F_MEM     = filt->memPtr();
-  float tmp_data[DATA_SIZE];
-  for (uint16_t i = 0; i < DATA_SIZE; i++) {
-    tmp_data[i] = *(F_MEM + ((i + LAST_SIDX) % DATA_SIZE));
-  }
-  draw_graph_obj(x, y, w, h, color, draw_base, draw_v_ticks, draw_h_ticks, tmp_data, (uint32_t) DATA_SIZE);
-}
-
-
-/*
-* Given a filter object, and parameters for the graph, draw the data to the
-*   display.
-*/
-void draw_progress_bar(
-  int x, int y, int w, int h, uint16_t color,
-  bool draw_base, bool draw_val, float percent
-) {
-  if (draw_base) {   // Draw the basic frame and axes?
-    display.fillRect(x, y, w, h, BLACK);
-    display.drawRoundRect(x, y, w, h, 3, WHITE);
-  }
-  uint8_t pix_width = percent * (w-2);
-  display.fillRoundRect(x+1, y+1, pix_width, h-2, 3, color);
-
-  if (draw_val && ((h-4) >= 7)) {
-    // If we have space to do so, and the application requested it, draw the
-    //   progress value in the middle of the bar.
-    int txt_x = x+3;
-    int txt_y = y+3;
-    display.setTextSize(0);
-    display.setCursor(txt_x, txt_y);
-    display.setTextColor(WHITE);
-    display.print(percent * 100.0);
-    display.print("%");
-  }
-}
-
-
-/*
-*/
-void draw_compass(
-  int x, int y, int w, int h,
-  bool scale_needle, bool draw_val, float bearing_field, float bearing_true_north
-) {
-  const int PERSPECTIVE_SCALE = 1;
-  int origin_x = x + (w >> 1);
-  int origin_y = y + (h >> 1);
-  int maximal_extent = (strict_min((int16_t) w, (int16_t) h) >> 1) - 1;
-  display.fillCircle(origin_x, origin_y, maximal_extent, BLACK);
-  display.drawCircle(origin_x, origin_y, maximal_extent, WHITE);
-  int needle_tip_n_x = (cos(bearing_field) * maximal_extent) + origin_x;
-  int needle_tip_n_y = (sin(bearing_field) * maximal_extent) + origin_y;
-  int needle_tip_s_x = needle_tip_n_x - (maximal_extent << 1);
-  int needle_tip_s_y = needle_tip_n_y - (maximal_extent << 1);
-  display.drawLine(origin_x, origin_y, needle_tip_n_x, needle_tip_n_y, RED);
-  display.drawLine(origin_x, origin_y, needle_tip_s_x, needle_tip_s_y, WHITE);
-  //display.fillTriangle(origin_x, origin_y, x1, y1, x2, y2, color);
-}
-
-
-/*
-* Given a vector object, and parameters for the graph, draw the data to the
-*   display. The given vector must be normalized.
-*/
-void draw_3vector(
-  int x, int y, int w, int h, uint16_t color,
-  bool draw_axes, bool draw_val, float vx, float vy, float vz
-) {
-  const int PERSPECTIVE_SCALE = 1;
-  int origin_x = x + (w >> 1);
-  int origin_y = y + (h >> 1);
-  if (draw_axes) {   // Draw the axes? The origin is in the middle of the field.
-    display.drawFastVLine(origin_x, y, h, WHITE);
-    display.drawFastHLine(x, origin_y, w, WHITE);
-    display.drawLine(x, (y+h), w, y, WHITE);
-    // Only 1/8 of a cube (all vector components are positive).
-    //display.drawFastVLine(x, y, h, WHITE);
-    //display.drawFastHLine(x, (y+h), w, WHITE);
-    //display.drawLine(x, (y+h), w>>1, y>>1, WHITE);
-  }
-  // Project the vector onto the x/y plane.
-  // To give a sense of depth, we use a triangle where only a line is required.
-  // We want the y-axis to be northward on the display. So we have to change the
-  //   sign of that component.
-  Vector3<int> projected(vx * (w >> 1), vy*(h >> 1) * -1, 0);   // TODO: z is unimplemented
-  int x1 = origin_x + projected.x - PERSPECTIVE_SCALE;
-  int y1 = origin_y + projected.y;
-  int x2 = origin_x + projected.x;
-  int y2 = origin_y + projected.y - PERSPECTIVE_SCALE;
-  display.fillTriangle(origin_x, origin_y, x1, y1, x2, y2, color);
-}
 
 
 /*
